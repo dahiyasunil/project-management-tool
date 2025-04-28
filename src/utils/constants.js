efe90d8f7e2cc96ce02d630c9d30769d779073a8
dahiyasunil <sunil.dahiya13@gmail.com>
2025-04-28 22:07:41 +0530

    feat: added constants for user role and task status

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const UserRolesEnum = {
   ADMIN: "admin",
   PROJECT_ADMIN: "project_admin",
-  member: "member",
+  MEMEBR: "member",
 };
 
 export const AvailableUserRoles = Object.values(UserRolesEnum);
