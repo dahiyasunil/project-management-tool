1a1e4ec7012524c8a45cb2db35353fcbd1d5aa05
dahiyasunil <sunil.dahiya13@gmail.com>
2025-04-28 22:51:51 +0530

    feat: added mongoose methods to generate access, refresh tokens and random token. Also added function to check if password is correct

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new Schema(
   {
@@ -62,5 +65,46 @@ userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+userSchema.methods.isPasswordCorrect = async function () {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.refreshAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+  const tokenExpiry = Date.now() + 20 * 60 * 1000; //20 min
+
+  return { hashedToken, unHashedToken, tokenExpiry };
+};
 
 export const User = mongoose.model("User", userSchema);
