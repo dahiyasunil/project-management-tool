a255e74b63124b5956096fffa7c32662e991db7d
dahiyasunil <sunil.dahiya13@gmail.com>
2025-04-28 22:10:10 +0530

    feat: added code for database connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import app from "./app.js"
+import app from "./app.js";
 import dotenv from "dotenv";
+import connectDb from "./db/index.js";
 
 dotenv.config("./.env");
 
 const PORT = process.env.PORT || 4000;
+
+connectDb().then(() =>
+  app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+);
