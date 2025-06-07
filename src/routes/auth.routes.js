dc7332196b8bbbf633345c057f8cf43dc0f8d8e5
dahiyasunil <sunil.dahiya13@gmail.com>
2025-06-07 19:40:31 +0530

    feat: register and verify user routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controllers.js";
+import { registerUser, verifyEmail } from "../controllers/auth.controllers.js";
 import { userRegistrationValidator } from "../validators/index.js";
 import { validate } from "../middlewares/validator.middlewares.js";
 
 const router = Router();
 
-router.route("/register").post(userRegistrationValidator(), validate, register);
+router
+  .route("/register")
+  .post(userRegistrationValidator(), validate, registerUser);
+
+router.route("/verify/:token").get(verifyEmail);
 
 export default router;
