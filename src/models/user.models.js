ac701f97ae0fb15ace3bbae3ef29393748655eda
dahiyasunil <sunil.dahiya13@gmail.com>
2025-06-07 19:32:55 +0530

    fix: missing next and password parameter

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -65,14 +65,14 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function () {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-userSchema.methods.isPasswordCorrect = async function () {
+userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
