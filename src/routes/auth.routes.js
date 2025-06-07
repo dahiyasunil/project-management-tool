ae2f929b35025371218c5071ce048eb4329b8f61
dahiyasunil <sunil.dahiya13@gmail.com>
2025-06-07 20:17:24 +0530

    feat: added login & logout routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,14 @@
 import { Router } from "express";
-import { registerUser, verifyEmail } from "../controllers/auth.controllers.js";
-import { userRegistrationValidator } from "../validators/index.js";
+import {
+  registerUser,
+  verifyEmail,
+  loginUser,
+  logoutUser,
+} from "../controllers/auth.controllers.js";
+import {
+  userRegistrationValidator,
+  userLoginValidator,
+} from "../validators/index.js";
 import { validate } from "../middlewares/validator.middlewares.js";
 
 const router = Router();
@@ -10,5 +18,9 @@ router
   .post(userRegistrationValidator(), validate, registerUser);
 
 router.route("/verify/:token").get(verifyEmail);
+
+router.route("/login").post(userLoginValidator(), validate, loginUser);
+
+router.route("/logout").get(logoutUser);
 
 export default router;
