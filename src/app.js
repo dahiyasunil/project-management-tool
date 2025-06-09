0d54bed17633c3f3b68af256315f6de52f5b66ab
dahiyasunil <sunil.dahiya13@gmail.com>
2025-06-09 23:13:42 +0530

    fix: fixed global error handler middleware , was creating new ApiError which was overiding stack strace

--- src/app.js
+++ src/app.js
@@ -9,22 +9,22 @@ app.use(cookieParser());
 //router imports
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
-import { ApiError } from "./utils/api-error.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use((err, req, res, next) => {
-  console.error(err.message);
-  res
-    .status(err.statusCode || 500)
-    .json(
-      new ApiError(
-        err.statusCode || 500,
-        err.message || "Internal Server Error",
-        err.errors || []
-      )
-    );
+  console.error(err);
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+    errors: err.errors || [],
+  });
 });
 
 export default app;
