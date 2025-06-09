1fba860b36690411582f8a87df61202aba49375a
dahiyasunil <sunil.dahiya13@gmail.com>
2025-06-10 00:05:45 +0530

    fix: wrapped ayncHandler in try catch, no need to explicitly define try catch for sync errors

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -1,8 +1,12 @@
 function asyncHandler(requestHandler) {
   return function (req, res, next) {
-    Promise.resolve(requestHandler(req, res, next)).catch(function (err) {
+    try {
+      Promise.resolve(requestHandler(req, res, next)).catch(function (err) {
+        next(err);
+      });
+    } catch (err) {
       next(err);
-    });
+    }
   };
 }
 
